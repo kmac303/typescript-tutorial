2baeafbf0926f0787ace20a5647a9519f33ccc2b
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2024-05-27 12:44:29 -0600

    tenth lesson complete - function signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,27 @@
-// sometimes specifying the type can get long winded like below
-// const logDetails = (uid: string | number, item: string) => {
-//     console.log(`${item} has a uid of ${uid}`);
-// }
+// let greet: Function;
 
-// const greet = (user: {name: string, uid: string | number}) => {
-//     console.log(`${user.name} says hello`);
-// }
-
-type StringorNum = string | number;
-type objWithName = { name: string, uid: StringorNum};
-
-const logDetails = (uid: StringorNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+// example 1
+let greet: (a: string, b: string) => void;
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
+// example 2
+let calc: (x: number, y: number, z: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add'){
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
 }
 
-const greetAgain = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
 }
