0905c4a6be1b94ffe9d37f5caba1be5464507650
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2024-05-29 19:03:50 -0600

    twentieth lesson complete - tuples

--- public/app.js
+++ public/app.js
@@ -12,32 +12,30 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// ENUMS
-var resourceType;
-(function (resourceType) {
-    resourceType[resourceType["BOOK"] = 0] = "BOOK";
-    resourceType[resourceType["AUTHOR"] = 1] = "AUTHOR";
-    resourceType[resourceType["FILM"] = 2] = "FILM";
-    resourceType[resourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    resourceType[resourceType["PERSON"] = 4] = "PERSON";
-})(resourceType || (resourceType = {}));
-const docOne = {
-    uid: 1,
-    resourceType: resourceType.BOOK,
-    data: { title: 'name of the wind' }
-};
-const docTwo = {
-    uid: 10,
-    resourceType: resourceType.PERSON,
-    data: { name: 'yoshi' }
-};
-console.log(docOne, docTwo);
+// tuples
+// the below array can be reassigned different types at different indexes
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+// tuple explicitly determines which type each index of the array is
+let tup = ['ryu', 25, true];
+tup[0] = 'ken';
+tup[1] = 30;
+// below won't work because index of 0 is a string, not a number
+// tup[0] = 25;
+let student;
+student = ['chun-li', 223434];
+// below won't work because string is first, number is second
+// student = [234324234, 'ken'];
--- src/app.ts
+++ src/app.ts
@@ -20,35 +20,35 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault(); 
 
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+
     let doc: HasFormatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     
     list.render(doc, type.value, 'end');
 });
 
-// ENUMS
+// tuples
 
-enum resourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+// the below array can be reassigned different types at different indexes
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-interface Resource<T> {
-    uid: number;
-    resourceType: ResourceType;
-    data: T;
-}
+// tuple explicitly determines which type each index of the array is
+let tup: [string, number, boolean] = ['ryu', 25, true];
+tup[0] = 'ken';
+tup[1] = 30;
+// below won't work because index of 0 is a string, not a number
+// tup[0] = 25;
 
-const docOne: Resource<object> = {
-    uid: 1,
-    resourceType: resourceType.BOOK,
-    data: { title: 'name of the wind' }
-}
-const docTwo: Resource<object> = {
-    uid: 10,
-    resourceType: resourceType.PERSON,
-    data: { name: 'yoshi' }
-}
-
-console.log(docOne, docTwo);
+let student: [string, number]; 
+student = ['chun-li', 223434];
+// below won't work because string is first, number is second
+// student = [234324234, 'ken'];
